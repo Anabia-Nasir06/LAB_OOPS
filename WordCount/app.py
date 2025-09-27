704de01c181c4f495a41fcd684cf444389844de9
Anabia Nasir <anabianasir.06@gmail.com>
2025-09-27 17:22:41 +0500

    OO version

--- WordCount/app.py
+++ WordCount/app.py
@@ -3,5 +3,14 @@ from wordcount import WordCount
 class App: 
     def run(self):
         print("==== WORD COOUNTER ====")
-        q = WordCount("apple orange apple banana apple orange")
-        print("Word counts:", q.word_counter())
+        # Initial query analysis
+        query = WordCount("best pizza best restaurant best service")
+        query.word_counter()
+        print(query.word_frequencies)
+
+
+        # Changing query
+        query.search_query = "grape mango grape"
+        query.word_counter()
+        print(analyzer.word_frequencies)
+
--- WordCount/wordcount.py
+++ WordCount/wordcount.py
@@ -1,10 +1,33 @@
 class WordCount:
-    def __init__(self, text):
-        self.text = text
+    def __init__(self, search_query):
+        self._search_query = search_query
+        self._word_list = self._search_query.split()
+        self._unique_words = []
+        self._word_frequencies = []
+
+    @property
+    def search_query(self):
+        return self._search_query
+
+    @search_query.setter
+    def search_query(self, value):
+        self._search_query = value
+        self._word_list = self._search_query.split()
+        self._unique_words = []
+        self._word_frequencies = []
 
     def word_counter(self):
-        words = self.text.split()
-        counts = {}
-        for w in words:
-            counts[w] = counts.get(w, 0) + 1
-        return counts
+        for word in self._word_list:
+            if word in self._unique_words:
+                index = self._unique_words.index(word)
+                self._word_frequencies[index] += 1
+            else:
+                self._unique_words.append(word)
+                self._word_frequencies.append(1)
+
+    @property
+    def word_frequencies(self):
+        return [[self._unique_words[i], self._word_frequencies[i]] for i in range(len(self._unique_words))]
+
+    def __str__(self):
+        return f"SearchQueryAnalyzer(search_query='{self._search_query}')"
